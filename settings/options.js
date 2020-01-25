311cac74ddb97356193cc4971f9fbfa326156973
Nathan Sprenkle <nathan.sprenkle@gmail.com>
2020-01-25 01:59:21 -0500

    Lint documents

--- settings/options.js
+++ settings/options.js
@@ -1,20 +1,20 @@
 /** Get blocked sites info from local storage */
-function getBlockedSites() {
+function getBlockedSites () {
   return JSON.parse(localStorage.blockedSites)
 }
 
 /** Create a table listing all the blocked site info */
-function displayBlockedSites(blockedSites) {
+function displayBlockedSites (blockedSites) {
   const blockedSitesSelector = 'blocked-sites'
   const fields = { siteName: 'Name', typeOfSite: 'Reason', registration: 'Origin' }
 
-  let sitesDisplay = document.getElementById(blockedSitesSelector)
+  const sitesDisplay = document.getElementById(blockedSitesSelector)
 
   // create header
-  let header = document.createElement('tr')
+  const header = document.createElement('tr')
 
   Object.keys(fields).forEach(field => {
-    let element = document.createElement('th')
+    const element = document.createElement('th')
     element.textContent = fields[field]
     header.append(element)
   })
@@ -23,20 +23,20 @@ function displayBlockedSites(blockedSites) {
 
   // add a row per site
   blockedSites.forEach(site => {
-    let row = document.createElement('tr')
+    const row = document.createElement('tr')
 
     Object.keys(fields).forEach(field => {
-      let element = document.createElement('td')
+      const element = document.createElement('td')
       element.textContent = site[field]
       row.append(element)
     })
 
     sitesDisplay.append(row)
-  });
+  })
 }
 
-function updateBlockedSitesCount(blockedSites) {
-  let blockedSitesCountSelector = "site-count"
+function updateBlockedSitesCount (blockedSites) {
+  const blockedSitesCountSelector = 'site-count'
   document.getElementById(blockedSitesCountSelector).textContent = blockedSites.length
 }
 
